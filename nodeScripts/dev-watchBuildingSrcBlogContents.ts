3b03a0bf0fa43cac247d3f726d17ff91ceb64d25
TerrySu <2684759855@qq.com>
2018-07-10 20:31:30 +0800

    Refactor: [Node scripts] Use typescript constants

--- nodeScripts/dev-watchBuildingSrcBlogContents.ts
+++ nodeScripts/dev-watchBuildingSrcBlogContents.ts
@@ -1,51 +1,39 @@
-import { KATEX_CSS_URL, KATEX_JS_URL } from "../../Terry-Su.github.io/scripts/constants/urls";
+import { CN, EN } from "../../Terry-Su.github.io/scripts/constants/names";
+import { GET_SHARED_NAV, GET_SHARED_DETAIL } from "../../Terry-Su.github.io/scripts/shared";
 
-const PATH = require("path")
-const {default: buildBlogContents} = require("../../BlogContentsBuilder/build/index")
-const gulp = require('gulp')
+const PATH = require( "path" )
+const {
+  default: buildBlogContents
+} = require( "../../BlogContentsBuilder/build/index" )
+const gulp = require( "gulp" )
 
-const origin = PATH.resolve(__dirname, "../src/staticBlogs")
-const output = PATH.resolve(__dirname, "../build")
+const origin = PATH.resolve( __dirname, "../src/staticBlogs" )
+const output = PATH.resolve( __dirname, "../build" )
 
 build()
-gulp.watch(`${ origin }/**`, build)
-
+gulp.watch( `${origin}/**`, build )
 
 function build() {
-  const textLogo =  'Custom'
-  const slogan = 'Custom slogan'
-  buildBlogContents(origin, output, {
-    nameOfDirectoryPlacingDataExceptNavHtml: 'blogsOutputExceptNavHtml',
-    // lang: 'cn',
-    lang: 'cn',
-
-
+  const lang = CN
+  // const lang = EN
+  const textLogo = "Custom"
+  const slogan = "Custom slogan"
+  buildBlogContents( origin, output, {
+    nameOfDirectoryPlacingDataExceptNavHtml: "blogsOutputExceptNavHtml",
+    lang,
     nav: {
+      ...GET_SHARED_NAV( lang ),
       textLogo,
       slogan,
-      scripts: [
-        '<script src="/scripts/nav.bundle.js"></script>'
-      ],
-      symbolUpdatingLocalstorage: new Date(),
-      
+      scripts                   : [ '<script src="/scripts/nav.bundle.js"></script>' ],
     },
-    
     detail: {
+      ...GET_SHARED_DETAIL( lang ),
       textLogo,
       slogan,
-      scripts: [
-        '<script src="/scripts/detail.bundle.js"></script>'
-      ],
-      commentRoot: 'https://api.github.com/repos/terry-su/tsblogcomments-en/issues/',
-      commentPageRoot: 'https://github.com/Terry-Su/TSBlogComments-En/issues/',
-      home: '/',
-      mathCssUrl: KATEX_CSS_URL,
-      mathJsUrl: KATEX_JS_URL,
-    },
+      scripts    : [ '<script src="/scripts/detail.bundle.js"></script>' ],
+    }
+  } )
 
-    
-    
-  })
-
-  console.log('built')
+  console.log( "built" )
 }
