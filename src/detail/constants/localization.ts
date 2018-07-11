d420f0a0a69d0255f7bd0759aa2cd1743ce5b2f8
TerrySu <2684759855@qq.com>
2018-07-11 12:31:48 +0800

    Fix: [Detail-Localization] BE_THE_FIRST_TO_COMMENT_ON_GITHUB

--- src/detail/constants/localization.ts
+++ src/detail/constants/localization.ts
@@ -1,6 +1,6 @@
-import { CN } from './../../shared/constants/names';
+import { CN } from "./../../shared/constants/names"
 import { getLangString } from "../../shared/commonUtils/lang"
-import { EN } from "../../shared/constants/names";
+import { EN } from "../../shared/constants/names"
 
 export const REPRINT_NOTE = getLangString( {
   [ EN ]: "Reprinting Note",
@@ -13,31 +13,27 @@ export const REPRINT_NOTE_DETAIL = getLangString( {
   [ CN ]: `作者版权所有，转载请注明出处，谢绝商业转载`
 } )
 
-
 export const HOME_PAGE = getLangString( {
-  [EN]: `Home`,
-  [CN]: `主页`
+  [ EN ]: `Home`,
+  [ CN ]: `主页`
 } )
-
 
 export const BE_THE_FIRST_TO_COMMENT = getLangString( {
-  [EN]: `Be the first to comment`,
-  [CN]: `撰写第一条评论`
+  [ EN ]: `Be the first to comment`,
+  [ CN ]: `撰写第一条评论`
 } )
 
-
 export const BE_THE_FIRST_TO_COMMENT_ON_GITHUB = getLangString( {
-  [EN]: `${BE_THE_FIRST_TO_COMMENT[EN]} on github`,
-  [CN]: `撰写第一条评论（使用Github）`
+  [ EN ]: `${ BE_THE_FIRST_TO_COMMENT } on github`,
+  [ CN ]: `撰写第一条评论（使用Github）`
 } )
 
 export const YOU_CAN_ALSO_COMMENT_ON_GITHUB = getLangString( {
-  [EN]: `You can also comment on github`,
-  [CN]: `撰写评论（使用Github）`
+  [ EN ]: `You can also comment on github`,
+  [ CN ]: `撰写评论（使用Github）`
 } )
 
 export const WRITE_A_COMMENT = getLangString( {
-  [EN]: `Write a comment`,
-  [CN]: `撰写评论`
+  [ EN ]: `Write a comment`,
+  [ CN ]: `撰写评论`
 } )
-
