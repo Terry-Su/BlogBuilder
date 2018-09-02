e56be601d82e9e4076169a4461e485e6f5de2550
TerrySu <2684759855@qq.com>
2018-09-03 00:38:33 +0800

    rename(TSBlogContentsBuilder): 'BlogContentsBuilder' to 'TSBlogContentsBuilder'

--- index.ts
+++ index.ts
@@ -1,25 +1,25 @@
-const {default: buildBlogContents} = require("../BlogContentsBuilder/build/index")
-const {PUBLISH_NAV_SCRIPT_DIRECTORY, PUBLISH_HOME_HTML_PATH} = require('./nodeScripts/constants/paths')
-const FS = require('fs-extra')
+const { default: buildBlogContents } = require( "../TSBlogContentsBuilder/build/index" )
+const { PUBLISH_NAV_SCRIPT_DIRECTORY, PUBLISH_HOME_HTML_PATH } = require( './nodeScripts/constants/paths' )
+const FS = require( 'fs-extra' )
 
 /**
  * Build blog
  * @param {*} root Original blogs' source path 
  * @param {*} output Output path 
  */
-module.exports = function(src, output, config={}) {
+module.exports = function( src, output, config = {} ) {
   const combinedConfig = {
     ...config
   }
-  buildBlogContents(src, output, combinedConfig)
-  buildNavHtml(output)
+  buildBlogContents( src, output, combinedConfig )
+  buildNavHtml( output )
 // buildScripts(output)
 }
 
-function buildNavHtml(output) {
-  FS.copyFileSync(PUBLISH_HOME_HTML_PATH, `${ output }/index.html`)
+function buildNavHtml( output ) {
+  FS.copyFileSync( PUBLISH_HOME_HTML_PATH, `${ output }/index.html` )
 }
 
-function buildScripts(output) {
-  FS.copySync(PUBLISH_NAV_SCRIPT_DIRECTORY, `${ output }/scripts`)
+function buildScripts( output ) {
+  FS.copySync( PUBLISH_NAV_SCRIPT_DIRECTORY, `${ output }/scripts` )
 }
--- server/dev-watchBuildingSrcBlogContents.ts
+++ server/dev-watchBuildingSrcBlogContents.ts
@@ -1,10 +1,10 @@
-import { CN, EN } from "../../Terry-Su.github.io/scripts/constants/names";
-import { GET_SHARED_NAV, GET_SHARED_DETAIL } from "../../Terry-Su.github.io/scripts/shared";
+import { CN, EN } from "../../Terry-Su.github.io/scripts/constants/names"
+import { GET_SHARED_NAV, GET_SHARED_DETAIL } from "../../Terry-Su.github.io/scripts/shared"
 
 const PATH = require( "path" )
 const {
   default: buildBlogContents
-} = require( "../../BlogContentsBuilder/build/index" )
+} = require( "../../TSBlogContentsBuilder/build/index" )
 const gulp = require( "gulp" )
 
 const origin = PATH.resolve( __dirname, "../src/staticBlogs" )
@@ -21,17 +21,17 @@ function build() {
   buildBlogContents( origin, output, {
     nameOfDirectoryPlacingDataExceptNavHtml: "blogsOutputExceptNavHtml",
     lang,
-    nav: {
+    nav                                    : {
       ...GET_SHARED_NAV( lang ),
       textLogo,
       slogan,
-      scripts                   : [ '<script src="/scripts/nav.bundle.js"></script>' ],
+      scripts: [ '<script src="/scripts/nav.bundle.js"></script>' ],
     },
     detail: {
       ...GET_SHARED_DETAIL( lang ),
       textLogo,
       slogan,
-      scripts    : [ '<script src="/scripts/detail.bundle.js"></script>' ],
+      scripts: [ '<script src="/scripts/detail.bundle.js"></script>' ],
     }
   } )
 
